6fa04575701a5a7eb7ae98de0b3be47e349de42e
codedipper <59625582+codedipper@users.noreply.github.com>
2020-08-14 14:12:51 -0400

    Created counter JavaScript.

--- counter.js
+++ counter.js
@@ -1,7 +1,7 @@
-const element = document.getElementById("time");
-
 function addTime(){
+  const element = document.getElementById("time");
   const currentTime = parseInt(element.innerHTML);
+  
   element.innerHTML = currentTime + 1;
 }
 
