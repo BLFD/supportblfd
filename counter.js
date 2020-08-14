f6f1e75a952f1d558fb3f6d970b9a495a1bd044f
codedipper <59625582+codedipper@users.noreply.github.com>
2020-08-14 14:30:19 -0400

    Deleted file.

--- counter.js
+++
@@ -1,8 +0,0 @@
-const element = document.getElementById("time");
-
-function addTime(){
-  const currentTime = parseInt(element.innerHTML);
-  element.innerHTML = currentTime + 1;
-}
-
-setInterval(addTime, 1000);
